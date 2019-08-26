f036240e18a67b09a0196a28ca0217473b51bcd0
mati <matilde.rivas@ug.uchile.cl>
2019-08-26 16:48:45 -0400

    Prints repeats that occur more than n times

--- main.cpp
+++ main.cpp
@@ -8,16 +8,32 @@
 using namespace std;
 using namespace sdsl;
 
-int main()
+int main(int argc, char* argv[])
 {
     cst_sct3<> cst;
+    int min_reps;
+    if (argc < 3){
+         min_reps = 2;
+    }
+    else {
+        char * pEnd;
+         min_reps = strtol(argv[2], NULL, 10);
+    }
     //string file ;
     //cin >> file;
     //construct(cst, file, 1);
     construct(cst, "/home/anouk/Documents/memoria/data/testSeq.fasta", 1);
-    cout << "inner nodes : " << cst.nodes() - cst.csa.size() << endl;
-    auto v = cst.select_child(cst.child(cst.root(), 'T'),1);
-    auto d = cst.depth(v);
-    cout << "v : " << d << "-[" << cst.lb(v) << "," << cst.rb(v) << "]" << endl;
-    cout << "extract(cst, v) : " << extract(cst, v) << endl;
+    //todo: get text with more than 1 non leaf child
+    for (auto it=cst.begin(); it!=cst.end(); ++it) {
+        if (cst.depth(*it) > 1 && it.visit() == 1) {  // node visited the first time
+            auto v = *it;       // get the node by dereferencing the iterator
+            if (cst.node_depth(v) >= 2 && cst.size(v) >= min_reps) {   // if depth of node is more than 1
+                // process node, e.g. output it in format d-[lb, rb]
+                cout << "node: " << extract(cst, v) << endl;
+                cout<<cst.depth(v)<<"-["<<cst.lb(v)<< ","<<cst.rb(v)<<"]"<<endl;
+            } else { // skip the subtree otherwise
+                it.skip_subtree();
+            }
+        }
+    }
 }
