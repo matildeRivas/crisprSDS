7c66d06250494ed74c234b329b2205a1708bdf70
mati <matilde.rivas@ug.uchile.cl>
2020-02-01 17:36:40 +0100

    Filters out candidates that are substrings of other ones

--- main.cpp
+++ main.cpp
@@ -14,10 +14,11 @@
 using namespace std;
 using namespace sdsl;
 
-int MIN_LENGTH = 2;
-int MAX_LENGTH = 3;
-int MIN_SPACER_LENGTH = 2;
-int MAX_SPACER_LENGTH = 3;
+//23 47 28 37
+int MIN_LENGTH = 23;
+int MAX_LENGTH = 47;
+int MIN_SPACER_LENGTH = 28;
+int MAX_SPACER_LENGTH = 37;
 
 // Compares two tuples according to their second element
 bool comp_by_second(const tuple<int, int> &a,
@@ -25,7 +26,13 @@ bool comp_by_second(const tuple<int, int> &a,
     return (get<1>(a) < get<1>(b));
 }
 
+// Compares two tuples according to their third element
+bool comp_by_third(const tuple<int, int, int, int> &a,
+                   const tuple<int, int, int, int> &b) {
+    return (get<2>(a) < get<2>(b));
+}
 
+// Constructs suffix array from string in given file
 int_vector<> create_sa(string infile) {
     int_vector<> seq;
     int32_t n;
@@ -46,28 +53,28 @@ int_vector<> create_sa(string infile) {
 // @candidate: text length, lower bound in SA, right bound in SA
 // returns candidates that meet the requirements for being CRISPR as a tuple containing
 // the number of repeats, their length, start and end position in text
-vector<tuple<int, int, int, int>> validate(tuple<int, int, int> candidate, int_vector<> sa, wt_int<> wt) {
+vector <tuple<int, int, int, int>> validate(tuple<int, int, int> candidate, int_vector<> sa, wt_int<> wt) {
     // obtain bounds
     int len = get<0>(candidate);
     auto lb = get<1>(candidate);
     auto rb = get<2>(candidate);
-    cout << len << " lb: " << lb << " rb: " << rb << endl;
-    vector<tuple<int, int>> cont_pairs;
+    //cout << len << " lb: " << lb << " rb: " << rb << endl;
+    vector <tuple<int, int>> cont_pairs;
     // for each candidate in group
     for (int c = lb; c <= rb; c++) {
         int sa_value = sa[c];
-        cout << "searching value after " << sa_value << " in range: " << sa_value + len + MIN_SPACER_LENGTH << " "
-             << sa_value + len + MAX_SPACER_LENGTH << endl;
+        // cout << "searching value after " << sa_value << " in range: " << sa_value + len + MIN_SPACER_LENGTH << " "
+        //     << sa_value + len + MAX_SPACER_LENGTH << endl;
         // search for next repeat in group that's within spacer range
         auto rs = wt.range_search_2d(lb, rb, sa_value + len + MIN_SPACER_LENGTH,
                                      sa_value + len + MAX_SPACER_LENGTH);
-        cout << "values found " << get<0>(rs) << endl;
+        //cout << "values found " << get<0>(rs) << endl;
         if (get<0>(rs) > 0) {
             for (int k = 0; k < get<0>(rs); k++) {
                 // tuple (position, value)
                 auto c = get<1>(rs).at(k);
                 cont_pairs.emplace_back(sa_value, (int) get<1>(rs).at(k).second);
-                cout << sa_value << " to " << get<1>(rs).at(k).second << endl;
+                // cout << sa_value << " to " << get<1>(rs).at(k).second << endl;
             }
         }
 
@@ -77,7 +84,8 @@ vector<tuple<int, int, int, int>> validate(tuple<int, int, int> candidate, int_v
 
     // key is the position of last occurence in chain so far
     // value stores number of occs, pos of first and last found so far
-    std::map<int, tuple<int, int, int>> chain_map;
+    std::map<int, tuple < int, int, int>>
+    chain_map;
     for (auto &p : cont_pairs) {
         int from = get<0>(p);
         int to = get<1>(p);
@@ -95,12 +103,14 @@ vector<tuple<int, int, int, int>> validate(tuple<int, int, int> candidate, int_v
         }
 
     }
-    vector<tuple<int, int, int, int>> crispr_list;
+
+    vector <tuple<int, int, int, int>> crispr_list;
     for (auto &x : chain_map) {
         // for each CRISPR we store the number of repeats, their length, start and end position
-        tuple<int, int, int, int> crispr = tuple<int, int, int, int>(get<0>(x.second), len, get<1>(x.second),
-                                                                     get<2>(x.second) + len - 1);
+        tuple<int, int, int, int> crispr = tuple<int, int, int, int>(get<0>(x.second), len, get<1>(x.second) + 1,
+                                                                     get<2>(x.second) + 1 + len);
         crispr_list.emplace_back(crispr);
+
     }
 
     return crispr_list;
@@ -116,14 +126,14 @@ int main(int argc, char *argv[]) {
         char *pEnd;
         min_reps = strtol(argv[2], NULL, 10);
     }
-    //string file ;
+    string file;
     //cin >> file;
-    //construct(cst, file, 1);
-    construct(cst, "/home/anouk/Documents/memoria/data/testSeq2.fasta", 1);
-    //construct_im(cst, "ATCGTACGTTCGAACT", 1);
+    //GI326314823
+    file =  "/home/anouk/Documents/memoria/data/GI326314823.fasta";
+    construct(cst, file, 1);
 
     // a candidate is a tuple: text length, lb, rb
-    vector<tuple<int, int, int>> candidate_list;
+    vector <tuple<int, int, int>> candidate_list;
     // iterate over all nodes
     for (auto it = cst.begin(); it != cst.end(); ++it) {
         if (cst.depth(*it) > 1 && it.visit() == 1) {  // node visited for the first time
@@ -144,19 +154,39 @@ int main(int argc, char *argv[]) {
         }
     }
     // the SA associated with the suffix tree
-    //int_vector<> sa = create_sa(file);
-    int_vector<> sa = create_sa("/home/anouk/Documents/memoria/data/testSeq2.fasta");
+    int_vector<> sa = create_sa(file);
+    //int_vector<> sa = create_sa("/home/anouk/Documents/memoria/data/GI326314823.fasta");
     // construct the wavelet tree using the SA
     wt_int<> wt;
     construct_im(wt, sa);
-    cout << sa << endl;
-    cout << wt << endl;
-    vector<tuple<int, int, int, int>> filtered_candidates;
+    //cout << sa << endl;
+    vector <tuple<int, int, int, int>> pre_filtered_candidates;
+    vector <tuple<int, int, int, int>> filtered_candidates;
 
     for (int i = 0; i < candidate_list.size(); i++) {
         for (auto &cr : validate(candidate_list[i], sa, wt))
-            filtered_candidates.emplace_back(cr);
-        //cout << get<0>(candidate_list[i]) << " " <<get<1>(candidate_list[i]) <<" " <<get<2>(candidate_list[i]) <<endl ;
+            pre_filtered_candidates.emplace_back(cr);
+    }
+
+    // deletes chains that are contained in another one, CHECK length
+
+    if (pre_filtered_candidates.size()) {
+         sort(pre_filtered_candidates.begin(), pre_filtered_candidates.end(), comp_by_third);
+         int checker = get<3>(pre_filtered_candidates[0]); // end position
+         filtered_candidates.emplace_back(pre_filtered_candidates[0]);
+         for (auto i = pre_filtered_candidates.begin(); i != pre_filtered_candidates.end(); ++i) {
+             if (get<3>(*i) > checker) {
+                 filtered_candidates.emplace_back(*i);
+                 checker = get<3>(*i);
+             }
+         }
+        // printing for debug reasons
+        for (auto &crispr_chain : filtered_candidates) {
+            cout << "number of reps " << get<0>(crispr_chain) << " length " << get<1>(crispr_chain) << " start pos "
+                 << get<2>(crispr_chain) <<
+                 " end pos " << get<3>(crispr_chain) << endl;
+        }
+
     }
 
 
